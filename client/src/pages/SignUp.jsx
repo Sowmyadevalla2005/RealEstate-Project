3adb191852b30674ba189e6f6bc9e936c0a7d077
kovali.kusam <kovali.kusam@tectoro.com>
2024-11-15 17:43:43 +0530

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -30,6 +30,7 @@ export default function SignUp() {
       if(data.success === false) {
         setLoading(false);
         setError(data.message);
+        return;
       }
       setLoading(false);
       setError(null);
