486034833022f668ede7fcdcde199b136ca5449e
kovali.kusam <kovali.kusam@tectoro.com>
2024-11-15 17:24:39 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -14,4 +15,23 @@ export const signup = async (req, res, next) => {
         next(error);
     }
     
+};
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});  // To check if email exists in database
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        // Secret is used to make token completely unique to our website
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc;
+        // saving token as cookie
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+    }
+    catch(error) {
+        next(error);
+    }
 };
