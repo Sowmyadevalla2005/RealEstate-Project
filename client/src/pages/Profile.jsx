41360c6f75def9081b32fde1666e6ab657286f07
kovali.kusam <kovali.kusam@tectoro.com>
2024-11-17 16:44:50 +0530

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'; // Import dispatch from Redux
 import { useState, useRef, useEffect } from 'react'; // Import useState and useEffect hooks
-import { deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
   const dispatch = useDispatch(); // Dispatch action
@@ -28,6 +28,24 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    }
+    catch(error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'> Profile </h1>
@@ -47,7 +65,7 @@ export default function Profile() {
 
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'> Delete account </span>
-        <span className='text-red-700 cursor-pointer'> Sign out </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'> Sign out </span>
       </div>
 
       <p className='text-red-700 mt-5'> 
